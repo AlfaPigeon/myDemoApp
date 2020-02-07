005f861aa698f295ac7fce31628ffc9c579c0071
oguzkaan.ozsoy@hotmail.com <oguzkaan.ozsoy@hotmail.com>
2020-02-08 00:32:48 +0300

    Done JUNIT tests -Oğuz kaan

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,5 +1,4 @@
 package com.mycompany.app;
-import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,23 +7,20 @@ import static junit.framework.Assert.*;
 
 
 public class AppTest {
-    @Test
+
     public void testFound() {
         ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
 
         assertTrue(new App().search(array, 4));
     }
-    @Test
     public void testNotFound() {
         ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         assertFalse(new App().search(array, 5));
     }
-    @Test
     public void testEmptyArray() {
         ArrayList<Integer> array = new ArrayList<>();
         assertFalse(new App().search(array, 1));
     }
-    @Test
     public void testNull() {
         assertFalse(new App().search(null, 1));
     }
